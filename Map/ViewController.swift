4537362e02d824c5384e1168a2682a3a5f350075
master <libaoming2@gmail.com>
2016-08-24 15:52:37 +0800

    complete longpress in mapview show annotation

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate{
 
+    @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude: CLLocationDegrees = 27.1
+        let longtitude: CLLocationDegrees = 78.0
+        
+        let latDelta: CLLocationDegrees = 0.05
+        let longDelta: CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.title = "Taj Mahal"
+        annotation.subtitle = "one day i will go there ..."
+        
+        annotation.coordinate = location
+        
+        map.addAnnotation(annotation)
+        
+        
+        
+        map.setRegion(region, animated: true)
+        
+        let uiLongPressGestureReconizer = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longPress(gesture:)))
+    
+        map.addGestureRecognizer(uiLongPressGestureReconizer)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    func longPress(gesture: UILongPressGestureRecognizer){
+        
+        let touchPoint = gesture.location(in: self.map)
+        
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = coordinate
+        
+        annotation.title = "new Place"
+        annotation.subtitle = "maybe i go there too"
+        
+        map.addAnnotation(annotation)
     }
-
-
 }
 
